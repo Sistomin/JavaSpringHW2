eb6c1e2e9fc7502fe0e238ac0479a9628f7b4cab
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2024-05-23 17:52:21 +0700

    Initial commit

---
+++ src/test/java/ru/gb/first_demo_project/MyFirstDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.first_demo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
